1c449fe37209705b841cd205552f2722d97bad8e
Nitish145 <royalnitish21@gmail.com>
2019-03-06 18:59:08 +0530

    Added Arduino Management

--- SkateBoard/Assets/Scripts/SimpleSkateController.cs
+++ SkateBoard/Assets/Scripts/SimpleSkateController.cs
@@ -7,8 +7,8 @@ public class SimpleSkateController : MonoBehaviour
     public Rigidbody rb;
     public float jumpForce = 10;
 
-    private float m_horizontalInput;
-    private float m_verticalInput;
+    
+
     private float m_steeringAngle;
 
     public WheelCollider FrontLeftC, FrontRightC;
@@ -20,6 +20,11 @@ public class SimpleSkateController : MonoBehaviour
     public float maxSteerAngle = 30;
     public float motorForce = 50;
 
+    public float m_rightHorizontalInput;
+    public float m_leftHorizontalInput;
+    public float m_upVerticalInput;
+    public float m_downVerticalInput;
+
     public bool isJump = false;
     private void Start()
     {
@@ -28,21 +33,55 @@ public class SimpleSkateController : MonoBehaviour
 
     public void GetInput()
     {
-        m_horizontalInput = Input.GetAxis("Horizontal");
-        m_verticalInput = -Input.GetAxis("Vertical");
+          m_rightHorizontalInput = Input.GetKeyDown(KeyCode.RightArrow) ? 1 : 0;
+          m_leftHorizontalInput = Input.GetKeyDown(KeyCode.LeftArrow) ? -1 : 0;
+          m_upVerticalInput = Input.GetKeyDown(KeyCode.UpArrow) ? -1 : 0;
+          m_downVerticalInput = Input.GetKeyDown(KeyCode.DownArrow) ? +1 : 0;
     }
 
     private void Steer()
     {
-        m_steeringAngle = maxSteerAngle * m_horizontalInput;
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+            m_steeringAngle = maxSteerAngle * m_rightHorizontalInput;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            m_steeringAngle = maxSteerAngle * m_leftHorizontalInput;
+
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+            m_steeringAngle = 0;
+
         FrontLeftC.steerAngle = m_steeringAngle;
         FrontRightC.steerAngle = m_steeringAngle;
     }
 
+    private void Deaccelerate()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            FrontLeftC.motorTorque = (m_downVerticalInput) * motorForce;
+            FrontRightC.motorTorque = (m_downVerticalInput) * motorForce;
+        }
+
+        if(Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            FrontLeftC.motorTorque = 0;
+            FrontRightC.motorTorque = 0;
+        }
+    }
+
     private void Accelerate()
     {
-        FrontLeftC.motorTorque = m_verticalInput * motorForce;
-        FrontRightC.motorTorque = m_verticalInput * motorForce;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            FrontLeftC.motorTorque = (m_upVerticalInput) * motorForce;
+            FrontRightC.motorTorque = (m_upVerticalInput) * motorForce;
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            FrontLeftC.motorTorque = 0;
+            FrontRightC.motorTorque = 0;
+        }
     }
 
     private void UpdateWheelPoses()
@@ -64,12 +103,31 @@ public class SimpleSkateController : MonoBehaviour
         _transform.rotation = _quat;
     }
 
+    private void AirControl()
+    {
+        if (isJump)
+        {
+            if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.AddForce(Vector3.left * 35 * 10000 * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.AddForce(Vector3.right * 35 * 10000 * Time.deltaTime);
+            }
+
+
+        }
+    }
+
     private void FixedUpdate()
     {
         GetInput();
         Steer();
+        Deaccelerate();
         Accelerate();
         UpdateWheelPoses();
+        AirControl();
 
         if (Input.GetKeyDown("space")) {
             if (!isJump)
@@ -77,14 +135,13 @@ public class SimpleSkateController : MonoBehaviour
                 isJump = true;
                 rb.AddForce(Vector3.up * jumpForce * Time.deltaTime * 50);
                 StartCoroutine(MultipleJump());
-                
             }
         }
     }
     IEnumerator MultipleJump()
     {
         print(isJump);
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(3);
         isJump = false;
     }
 
